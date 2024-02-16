85f7b4aea775a497a73ae886c54426d45da39e53
useongee <118250509+USEONGEE@users.noreply.github.com>
2024-02-16 22:44:12 +0900

    feat: 이거까지

--- src/main/java/com/wagle/backend/domain/post/domain/Post.java
+++ src/main/java/com/wagle/backend/domain/post/domain/Post.java
@@ -1,6 +1,8 @@
 package com.wagle.backend.domain.post.domain;
 
 import com.wagle.backend.domain.member.domain.Member;
+import com.wagle.backend.domain.post.dto.PostCreateDto;
+import com.wagle.backend.domain.post.dto.PostUpdateDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,13 +18,29 @@ public class Post {
     @Id
     @GeneratedValue
     private Long id;
+    private Long memberId;
     private String name;
     @Lob
     private String content;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+    // JPA 연관관계를 속도의 문제로 인해 걸지 않는다.
     private Double latitude;
     private Double longitude;
+
+    public static Post ofPost(PostCreateDto postCreateDto) {
+        return Post.builder()
+                .memberId(postCreateDto.getMemberId()) // 이게 없는건 애초에 Auth가 없는거 말이 안된다.
+                .name(postCreateDto.getName())
+                .content(postCreateDto.getContent())
+                .latitude(postCreateDto.getLatitude())
+                .longitude(postCreateDto.getLongitude())
+                .build();
+    }
+
+    public void update(PostUpdateDto postUpdateDto) {
+        this.name = postUpdateDto.getName();
+        this.content = postUpdateDto.getContent();
+        this.latitude = postUpdateDto.getLatitude();
+        this.longitude = postUpdateDto.getLongitude();
+    }
 }
