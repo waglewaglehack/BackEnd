9962b00cc4fe6370cd69a6646bca1cb6c209b30f
useongee <118250509+USEONGEE@users.noreply.github.com>
2024-02-16 22:45:10 +0900

    feat: PostController 작업중

--- src/main/java/com/wagle/backend/domain/post/controller/PostController.java
+++ src/main/java/com/wagle/backend/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.wagle.backend.domain.post.controller;
 
 import com.wagle.backend.domain.member.domain.Member;
 import com.wagle.backend.domain.post.domain.Post;
+import com.wagle.backend.domain.post.dto.PostCreateDto;
 import com.wagle.backend.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,9 @@ public class PostController {
         return ResponseEntity.ok(findPost);
     }
 
-    @PostMapping
-    public ResponseEntity save(@AuthenticationPrincipal("member") Member member,
-                               @RequestBody PostCrteat)
+//    @PostMapping
+//    public ResponseEntity save(@AuthenticationPrincipal("member") Member member,
+//                               @RequestBody PostCreateDto postCreateDto) {
+//        return null;
+//    }
 }
